b799ec5f2ec226cbfdad3164acd331e6537d105d
dplayerf <118938881+dplayerf@users.noreply.github.com>
2024-09-10 00:12:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+    {
+        titulo: "Lenilson",
+        descricao: "Nascido em São Sebastião de Lagoa de Roça - PB, Influxking é um dos membros mais ativos do servidor Gangue do ventilador, sendo o ADM lider e sem saber dar 200 no fortnite ele é um dos membros mais importantes da gangue do ventilador e fundador da mesma.",
+        link: "https://www.instagram.com/lenil272",
+        tags: "gay amoeba player banana"
+    },
+    {
+        titulo: "João",
+        descricao: "Nascido no Paraná, João é um dos membros mais ativos no servidor, conhecido por adorar animes de garotinhas que tocam e cantam rock ele também é um dos melhores players de AMQ e Fortnite atualmente. Esse cara também é um dos ADMs do servidor e também a webnamorada do influxking.",
+        link: "https://www.instagram.com/jaoxn99",
+        tags: "insuportavel chato feio",
+    },
+    {
+        titulo: "Dplayerf",
+        descricao: "Nascido em São Sebastião de Lagoa de Roça - PB, Dp como também o chamam é um membro ativo e não ativo do servidor Gangue do ventilador, conhecido por sua fama de sonegar impostos, esse membro é considerado um dos fundadores do servidor e um pilar entre todos, por ter seu humor duvidoso e esquisito ele é um cara muito gente boa, lindo e atraente e possui a skill namorada",
+        link: "https://www.instagram.com/daniel2mota",
+        tags: "lindo maravilhoso Daniel daniel dp gostoso gente boa",
+    },
+    {
+        titulo: "Tavin",
+        descricao: "Sendo um dos membros bem ativo na gangue do ventilador Tavin é um mineiro muito conhecido e querido dentro da gangue, com suas personalidades duvidosas ele é um otimo piadista, enviando suas imagens pertubadoras no chat e esquisitas ele faz fama com sua personalidade.",
+        link: "https://pt.wikipedia.org/wiki/Orangotango",
+        tags: "orangutango banana fortnite tavo tavinhas",
+
+    },
+    {
+        titulo: "Iago",
+        descricao: "Nascido em São Sebastião de Lagoa de Roça - PB, Iago o gai, é um dos membros ativos do servidor, conhecido por ser um cara estranho mas que nós nos acostumamos com ele, ele é a criatura mais rara da especie humana, com um shape de academia um pouco duvidoso levando o fato que ele faz academia a muito tempo.",
+        link:  "https://www.instagram.com/iag0fer",
+        tags: "o gai jesus Jesus we live we love we lie",
+    },
+    {
+        titulo: "Drajinhas",
+        descricao: "Nascido em São Paulo - SP, Drajinhas é um recém-chegado membro do servidor Gangue do Ventilador. Sendo um dos novatos do grupo, costuma jogar Fortnite, dar 200 e se divertir enquanto joga com os amigos.",
+        link: "https://pt.wikipedia.org/wiki/Capivara",
+        tags: "capivara drajas drajis novato são paulo",
+    },
+    {
+        titulo: "Mario",
+        descricao: "Mario é um dos membros da gangue do ventilador de certa forma antigo mas sua origem dentro do grupo é bem suspeito, ele chegou apenas como um membro no qual o dp chamou para o grupoe e acabou virando nosso amigo KKKKKKKKKKKfds tlgd.",
+        link: "https://pt.wikipedia.org/wiki/Candiru",
+        tags: "candiru esquisito suicidio minecraft vingança",
+    }
+];
